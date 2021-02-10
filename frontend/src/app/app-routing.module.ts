b58bfe8a79df62c79c15982b68b2543b154b4201
Alex Kvasha <alex.kvasha228@gmail.com>
2021-02-10 18:25:15 +0300

    Add home component with first layout

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  
+  { path: "", component: HomeComponent, pathMatch: "full" },
+  { path: "home", redirectTo: "/" }
 ];
 
 @NgModule({
