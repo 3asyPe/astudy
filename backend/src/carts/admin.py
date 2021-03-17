084149fad79b9264488a31a2107cbac3218e0c9e
Alex Kvasha <alex.kvasha228@gmail.com>
2021-03-17 14:00:39 +0300

    Update Cart's admin

--- backend/src/carts/admin.py
+++ backend/src/carts/admin.py
@@ -3,4 +3,42 @@ from django.contrib import admin
 from .models import Cart
 
 
-admin.site.register(Cart)
+class UserFilter(admin.SimpleListFilter):
+    title = "User"
+    parameter_name = "user"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("has_user", "Has a user"),
+            ("no_user", "Doesn't have a user"),
+        )
+
+    def queryset(self, request, queryset):
+        if not self.value():
+            return queryset
+        if self.value().lower() == "has_user":
+            return queryset.exclude(user=None)
+        if self.value().lower() == "no_user":
+            return queryset.filter(user=None)
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 
+        'user', 
+        'total', 
+        'active',
+    ]
+
+    list_display_links = [
+        'id',
+        'user',
+        'total',
+        'active'
+    ]
+
+    list_filter = [
+        'active',
+        UserFilter
+    ]
