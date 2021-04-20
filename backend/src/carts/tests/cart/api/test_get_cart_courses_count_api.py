1f2637e2cac198664e97e9950bf1a804f27aaf5b
Alex Kvasha <alex.kvasha228@gmail.com>
2021-04-20 19:03:46 +0300

    Add tests for get cart courses count api

---
+++ backend/src/carts/tests/cart/api/test_get_cart_courses_count_api.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+pytestmark = [pytest.mark.django_db]
+
+
+def test_get_courses_count_api(api, cart):
+    response = api.get("/api/cart/count/", {
+        "cart_id": cart.id,
+    })
+
+    assert response["cart_id"] == cart.id
+    assert response["cart_courses_count"] == cart.courses.count()
+
+
+def test_get_courses_count_api_call_method(mocker, api, cart):
+    get_courses_count = mocker.patch(
+        "carts.models.Cart.get_courses_count", 
+        return_value=cart.courses.count()
+    )
+    response = api.get("/api/cart/count/", {
+        "cart_id": cart.id,
+    })
+
+    assert get_courses_count.called_once()
+
