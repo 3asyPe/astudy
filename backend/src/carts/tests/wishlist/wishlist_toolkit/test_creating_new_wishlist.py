bdba1c059f9ffc147686266310a375ffd684befd
Alex Kvasha <alex.kvasha228@gmail.com>
2021-04-27 15:49:00 +0300

    Add tests for creating new wishlist method

--- backend/src/carts/tests/wishlist/wishlist_toolkit/test_creating_new_wishlist.py
+++ backend/src/carts/tests/wishlist/wishlist_toolkit/test_creating_new_wishlist.py
@@ -1,7 +1,10 @@
 import pytest
 
+from accounts.utils import AccountErrorMessages
+from app.errors import ValidationError
 from carts.models import Wishlist
 from carts.services import WishlistToolkit
+from carts.utils import WishlistErrorMessages
 
 
 pytestmark = [pytest.mark.django_db]
@@ -20,8 +23,21 @@ def test_create_new_wishlist(mocker, user, another_user):
         return_value=wishlist
     )
 
-    WishlistToolkit.create_new_wishlist(user=user)
+    got = WishlistToolkit.create_new_wishlist(user=user)
 
     create_wishlist.assert_called_once()
 
 
+def test_create_new_wishlist_with_anonymous_user(anonymous_user):
+    with pytest.raises(PermissionError) as exc:
+        got = WishlistToolkit.create_new_wishlist(user=anonymous_user)
+    
+    assert str(exc.value) == AccountErrorMessages.USER_IS_NOT_AUTHENTICATED_ERROR.value
+
+
+def test_create_already_existed_wishlist(user):
+    wishlist = Wishlist.objects.create(user=user)
+    with pytest.raises(ValidationError) as exc:
+        got = WishlistToolkit.create_new_wishlist(user=user)
+    
+    assert str(exc.value) == WishlistErrorMessages.WISHLIST_ALREADY_EXISTS_ERROR.value
