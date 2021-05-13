a6043a891dc683603eefea10a032df225c46d395
Alex Kvasha <alex.kvasha228@gmail.com>
2021-05-13 20:33:21 +0300

    Add tests for apply coupon method

---
+++ backend/src/discounts/tests/toolkit/test_apply_coupon.py
@@ -0,0 +1,44 @@
+import pytest
+
+from decimal import Decimal
+
+from app.errors import ValidationError
+from discounts.services import CouponToolkit
+from discounts.utils import DiscountErrorMessages
+
+
+pytestmark = [pytest.mark.django_db]
+
+
+@pytest.fixture
+def cart(cart, course_factory):
+    cart.courses.add(course_factory())
+    cart.courses.add(course_factory())
+    return cart
+
+
+@pytest.fixture
+def coupon(coupon):
+    coupon.discount = 10
+    coupon.save()
+    return coupon
+
+
+def test_applying_valid_coupon(coupon, cart):
+    coupon.applicable_to.add(cart.courses.first())
+    applied_coupon = CouponToolkit.apply_coupon(code=coupon.code, cart=cart)
+
+    assert applied_coupon.coupon == coupon
+    assert applied_coupon.cart == cart
+    assert applied_coupon.active == True
+
+    assert cart.subtotal == cart.courses.first().price + cart.courses.last().price
+    assert cart.total == round(cart.courses.first().price * Decimal('0.9'), 2) + cart.courses.last().price
+
+
+def test_applying_invalid_coupon(cart):
+    with pytest.raises(ValidationError) as exc:
+        CouponToolkit.apply_coupon(code="WRONGCODE", cart=cart)
+    
+    assert str(exc.value) == DiscountErrorMessages.INVALID_APPLIED_COUPON_ERROR.value
+    assert cart.total == cart.subtotal == cart.courses.first().price + cart.courses.last().price
